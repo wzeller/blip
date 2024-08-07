a650ff2961035bf4e6801bd522331edf3b03026a
William Zeller <williamzeller@Williams-MacBook-Pro.local>
2024-08-07 12:52:39 -0700

    Comment out local.example.js

--- config/local.example.js
+++ config/local.example.js
@@ -1,51 +1,51 @@
-/**
- * Copy this file to `config/local.js` and update as needed
- */
+// /**
+//  * Copy this file to `config/local.js` and update as needed
+//  */
 
-/**
- * Uncomment any linkedPackages as desired to link them for local development.
- *
- * These will be resolved as aliases in the webpack config. Note that you will need to ensure that
- * the packages are installed (via `yarn install`) in each respective folder
- *
- * It's recommended to use the `yarn startLocal` script to run the app, as it will automatically
- * start the webpack development server for the `viz` repo when needed.
- *
- * You may add as other modules to this list as well.
- */
-const linkedPackages = {
-  // '@tidepool/viz': process.env.TIDEPOOL_DOCKER_VIZ_DIR || '../viz',
-  // 'tideline': process.env.TIDEPOOL_DOCKER_TIDELINE_DIR || '../tideline',
-  // 'tidepool-platform-client': process.env.TIDEPOOL_DOCKER_PLATFORM_CLIENT_DIR || '../platform-client',
-};
+// /**
+//  * Uncomment any linkedPackages as desired to link them for local development.
+//  *
+//  * These will be resolved as aliases in the webpack config. Note that you will need to ensure that
+//  * the packages are installed (via `yarn install`) in each respective folder
+//  *
+//  * It's recommended to use the `yarn startLocal` script to run the app, as it will automatically
+//  * start the webpack development server for the `viz` repo when needed.
+//  *
+//  * You may add as other modules to this list as well.
+//  */
+// const linkedPackages = {
+//   // '@tidepool/viz': process.env.TIDEPOOL_DOCKER_VIZ_DIR || '../viz',
+//   // 'tideline': process.env.TIDEPOOL_DOCKER_TIDELINE_DIR || '../tideline',
+//   // 'tidepool-platform-client': process.env.TIDEPOOL_DOCKER_PLATFORM_CLIENT_DIR || '../platform-client',
+// };
 
-const featureFlags = {
-  i18nEnabled: false,
-  pendoEnabled: true,
-  rxEnabled: false,
-};
+// const featureFlags = {
+//   i18nEnabled: false,
+//   pendoEnabled: true,
+//   rxEnabled: false,
+// };
 
-const environments = {
-  local: 'http://localhost:31500',
-  dev: 'https://dev1.dev.tidepool.org',
-  qa1: 'https://qa1.development.tidepool.org',
-  qa2: 'https://qa2.development.tidepool.org',
-  qa3: 'https://qa3.development.tidepool.org',
-  qa4: 'https://qa4.development.tidepool.org',
-  qa5: 'https://qa5.development.tidepool.org',
-  int: 'https://int-app.tidepool.org',
-  prd: 'https://app.tidepool.org',
-};
+// const environments = {
+//   local: 'http://localhost:31500',
+//   dev: 'https://dev1.dev.tidepool.org',
+//   qa1: 'https://qa1.development.tidepool.org',
+//   qa2: 'https://qa2.development.tidepool.org',
+//   qa3: 'https://qa3.development.tidepool.org',
+//   qa4: 'https://qa4.development.tidepool.org',
+//   qa5: 'https://qa5.development.tidepool.org',
+//   int: 'https://int-app.tidepool.org',
+//   prd: 'https://app.tidepool.org',
+// };
 
-const apiHost = environments.dev;
-const launchDarklyClientToken = '';
-const uploadApi = apiHost;
+// const apiHost = environments.dev;
+// const launchDarklyClientToken = '';
+// const uploadApi = apiHost;
 
-module.exports = {
-  listLinkedPackages: () => console.log(Object.keys(linkedPackages).join(',')),
-  linkedPackages,
-  featureFlags,
-  apiHost,
-  launchDarklyClientToken,
-  uploadApi,
-};
+// module.exports = {
+//   listLinkedPackages: () => console.log(Object.keys(linkedPackages).join(',')),
+//   linkedPackages,
+//   featureFlags,
+//   apiHost,
+//   launchDarklyClientToken,
+//   uploadApi,
+// };
